2e1c80ae7f72a04d817c99cc9b60f605c0698257
Favenzio Calvo <fcalvo@fsu.edu>
2022-08-10 20:31:04 -0400

    Removed conflicting jquery calls.

--- strata-3.1.6/_/inc/close.php
+++ strata-3.1.6/_/inc/close.php
@@ -1,13 +1,6 @@
 
-<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js?ver=20150709'></script>
-<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js?ver=20150709'></script>
 <script type="text/javascript" src="/_/s3/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/_/s3/js/strata.min.js"></script>
-
-<script type="text/javascript" src="/_/s3/js/extend/jquery.flexslider.min.js"></script>
-<script type="text/javascript" src="/_/s3/js/extend/flexslider.min.js"></script>
-<script type="text/javascript" src="/_/s3/js/extend/strata-carousel.min.js"></script>
-
 <script type="text/javascript" src="/_/local/js/strata-styleguide.min.js"></script>
 
 </body>
